ee7d9a46b724b9d67d14e34f2db6474cb42ac9e9
Medranomiler1 <medranomiler@gmail.com>
2023-04-13 22:46:40 -0500

    updated booking page

--- src/components/Calendly.js
+++ src/components/Calendly.js
@@ -1,24 +1,13 @@
 import React, { useEffect } from 'react';
 
 function Calendly() {
-  useEffect(() => {
-    const head = document.querySelector('head');
-    const script = document.createElement('script');
-    script.setAttribute('src', 'https://assets.calendly.com/assets/external/widget.js');
-    head.appendChild(script);
-
-    return () => {
-      head.removeChild(script);
-    };
-  }, []);
 
   return (
-    <div
-      className="calendly-inline-widget"
-      data-url="https://calendly.com/mikedropfades/haircut-40?month"
-      style={{ minWidth: '320px', height: '900px' }}
-    />
+    <div>
+    <iframe src="https://calendly.com/mikedropfades/haircut-40?month" className="w-full h-[655px]"></iframe></div>
   );
 }
 
 export default Calendly;
+
+
