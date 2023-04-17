be783831691ebca7593a34aa48c6afca33cff532
Medranomiler1 <medranomiler@gmail.com>
2023-04-17 15:26:57 -0500

    removed dates from gallery

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -3,11 +3,11 @@ import React from 'react'
 const Gallery = ({feed}) => {
   const images = feed.data
 
-  function formattedDate(timestamp) {
-    const date = new Date(timestamp);
-    const options = { month: '2-digit', day: '2-digit', year: 'numeric' };
-    return date.toLocaleDateString('en-US', options);
-  }
+  // function formattedDate(timestamp) {
+  //   const date = new Date(timestamp);
+  //   const options = { month: '2-digit', day: '2-digit', year: 'numeric' };
+  //   return date.toLocaleDateString('en-US', options);
+  // }
 
   return (
         <main className="flex flex-wrap justify-center sm:p-4">
@@ -22,7 +22,7 @@ const Gallery = ({feed}) => {
               )}
               <div className="p-2 flex flex-col items-center">
               <h2 className="text-white mt-1 text-sm font-bold">{image.caption}</h2>
-              <p className="text-white mt-1 text-sm font-bold">{formattedDate(image.timestamp)}</p>
+              {/* <p className="text-white mt-1 text-sm font-bold">{formattedDate(image.timestamp)}</p> */}
               </div>
           </div>
         ))}
